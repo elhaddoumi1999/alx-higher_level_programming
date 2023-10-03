1d0f9d8a5de33f456adaa1a80d3f7f5e3b7033ec
elhaddoumi <elhaddoumi99@gmail.com>
2023-10-03 15:03:17 -0700

    Remove dir

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3
-# Author - EL HADDOUMI Mohammed
-for letter in range(97, 123):
-    if chr(letter) is not 'q' and chr(letter) is not 'e':
-        print("{}".format(chr(letter)), end="")
