176653a0470fbff482f2868702940b4a3398de89
elhaddoumi <elhaddoumi99@gmail.com>
2023-10-03 16:07:59 -0700

    Update 3-print_alphabt.p filey

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 # Author - EL HADDOUMI Mohammed
 for letter in range(97, 123):
-    if chr(letter) is not 'q' and chr(letter) is not 'e':
+    if chr(letter) != 'q' and chr(letter) != 'e':
         print("{}".format(chr(letter)), end="")
