101d4c2f5198cf0ea557d308a28505996fe1bdc1
elhaddoumi <elhaddoumi99@gmail.com>
2023-10-05 12:09:03 -0700

    Update file 102-magic_calculation.py

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+# 102-magic_calculation.py
+# Auther : EL HADDOUMI Mohammed
+
 
 def magic_calculation(a, b):
+    """Match bytecode provided by Holberton School."""
     from magic_calculation_102 import add, sub
 
     if a < b:
