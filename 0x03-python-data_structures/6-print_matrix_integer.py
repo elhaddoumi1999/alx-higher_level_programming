61edf013df38427d629af117dbf4461c07042c80
elhaddoumi <elhaddoumi99@gmail.com>
2023-10-07 12:00:20 -0700

    Add add 6-print_matrix_integer.py  file

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+#Auth: EL HADDOUMI Mohammed
+def print_matrix_integer(matrix=[[]]):
+    l = len(matrix)
+    for x in range(l):
+        for y in range(len(matrix[x])):
+            print('{:d}'.format(matrix[x][y]), end='')
+            if y is not (len(matrix[x]) - 1):
+                print(end=" ")
+        print("")
