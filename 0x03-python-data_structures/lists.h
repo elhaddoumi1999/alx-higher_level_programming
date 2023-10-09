5f08b2081eabfbbdb0f37b66c808d515b2dc6dd3
elhaddoumi <elhaddoumi99@gmail.com>
2023-10-09 15:24:01 -0700

    update

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -1,6 +1,6 @@
 #ifndef LISTS_H
 #define LISTS_H
-
+#include <stdio.h>
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -11,8 +11,8 @@
  */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
@@ -21,4 +21,4 @@ void free_listint(listint_t *head);
 
 int is_palindrome(listint_t **head);
 
-#endif /* LISTS_H */
+#endif
