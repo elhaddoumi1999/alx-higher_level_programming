628756c89d1ca7d98c635d64c37e537e2e74ba78
elhaddoumi <elhaddoumi99@gmail.com>
2023-10-11 17:39:41 -0700

    Add All files

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new = []
-    for x in my_list:
-        if x == search:
-            new.append(replace)
-        else:
-            new.append(x)
-    return (new)
+    def s_r_elm(elm):
+        return (elm if elm != search else replace)
+    return list(map(s_r_elm, my_list))
--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-def replace_in_list(my_list, idx, element):
-    if idx < 0 or idx > len(my_list) - 1:
-        return my_list
-    else:
-        my_list[idx] = element
-        return my_list
+def search_replace(my_list, search, replace):
+    def s_r_elm(elm):
+        return (elm if elm != search else replace)
+    return list(map(s_r_elm, my_list))
