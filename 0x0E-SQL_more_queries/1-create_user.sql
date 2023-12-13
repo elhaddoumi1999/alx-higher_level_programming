5d568c910a5371f4f2baeb3a319f39774544e03b
elhaddoumi <elhaddoumi99@gmail.com>
2023-12-13 19:08:47 +0100

    0x0E. SQL - More queries

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,9 +1,5 @@
--- creates MySQL user user_0d_1 with all priviledges
-CREATE USER
-       IF NOT EXISTS 'user_0d_1'@'localhost'
-       IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES
-      ON *.*
-      TO 'user_0d_1'@'localhost'
-      IDENTIFIED BY 'user_0d_1_pwd';
-FLUSH PRIVILEGES;
+-- Create A USER
+CREATE USER IF NOT EXISTS
+user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON
+*.* TO user_0d_1@localhost;
--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,11 +1,4 @@
--- creates the database hbtn_0d_2 and user_0d_2
-CREATE DATABASE
-       IF NOT EXISTS `hbtn_0d_2`;
-CREATE USER
-       IF NOT EXISTS 'user_0d_2'@'localhost'
-       IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT
-      ON `hbtn_0d_2`.*
-      TO 'user_0d_2'@'localhost'
-      IDENTIFIED BY 'user_0d_2_pwd';
-FLUSH PRIVILEGES;
+-- Create a datbase and and user with Select only privlage
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost; 
