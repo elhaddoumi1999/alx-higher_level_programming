80688e798b9649a2c91046c6acd01fa2f53e97cd
elhaddoumi <elhaddoumi99@gmail.com>
2023-12-13 19:11:04 +0100

    0x0E. SQL - More queries

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,4 @@
 -- Create a datbase and and user with Select only privlage
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-CREATE USER user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost; 
+CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost
