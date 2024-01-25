267da7c031c44bc6cfce41dd37621537496c0c3f
elhaddoumi <elhaddoumi99@gmail.com>
2024-01-25 19:17:07 +0100

    0x10. Python - Network #0

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
-"""Algorithms for list of integers."""
+""" Finds peak in list of unsorted integers"""
 
 
 def find_peak(list_of_integers):
-    """Finds a peak in a list of unsorted integers."""
-    if list_of_integers:
-        list_of_integers.sort(reverse=True)
-        return list_of_integers[0]
+    """Find highest value in list of unsorted integers"""
+
+    my_list = list_of_integers
+
+    if my_list:
+        my_list.sort()
+        return my_list[-1]
+    else:
+        return None
