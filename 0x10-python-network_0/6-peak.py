64e94df2e23c151a2186872ffc49fa8a8197359b
elhaddoumi <elhaddoumi99@gmail.com>
2024-01-25 19:20:03 +0100

    0x10. Python - Network #0

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
-""" Finds peak in list of unsorted integers"""
-
 
 def find_peak(list_of_integers):
-    """Find highest value in list of unsorted integers"""
-
-    my_list = list_of_integers
-
-    if my_list:
-        my_list.sort()
-        return my_list[-1]
-    else:
+    if not list_of_integers:
         return None
+
+    low, high = 0, len(list_of_integers) - 1
+
+    while low < high:
+        mid = (low + high) // 2
+
+        if list_of_integers[mid] >= list_of_integers[mid + 1]:
+            high = mid
+        else:
+            low = mid + 1
+
+    return list_of_integers[low]
