cecc9fea9754446c419d5b8a47af7151b2eca4f9
elhaddoumi <elhaddoumi99@gmail.com>
2024-02-20 18:34:20 +0100

    0x14. JavaScript - Web scraping

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,14 +1,6 @@
 #!/usr/bin/node
 const request = require('request');
-
-const movie_id = process.argv[2];
-
-if (parseInt(movie_id) < 8) {
-    const url = 'https://swapi-api.hbtn.io/api/films/' + movie_id;
-    request(url, function (err, response, body) {
-	if (err) {
-	    return console.log(err);
-	}
-	console.log(JSON.parse(body).title);
-    });
-}
+const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
+request(url, function (error, response, body) {
+  console.log(error || JSON.parse(body).title);
+});
